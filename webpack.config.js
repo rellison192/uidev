11152d02f347b727f4355d0a669c59769269cc7b
rellison192 <rellison192@g.rwu.edu>
2022-09-24 11:02:02 -0400

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+  },
+  module: {
+    rules: [
+      {test: /\.(js|jsx)$/, use: 'babel-loader'},
+      {test: /\.css$/, use: ['style-loader', 'css-loader']},
+    ],
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.html',
+    }),
+    new Dotenv(),
+  ],
+};
