86015b9083a8005b804bb2468b41724eaa6184ea
Ryan Ellison <rellison192@g.rwu.edu>
2022-09-30 11:54:11 -0400

    react course - 2 popular navbar

--- react-course/app/index.js
+++ react-course/app/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDom from 'react-dom/client'
+import Popular from './components/Popular'
 import './index.css'
 
 class App extends React.Component {
   render() {
-    return <div>hello</div>
+    return (
+      <div className="Light">
+        <div className="container">
+          <Popular />
+        </div>
+      </div>
+    )
   }
 }
 const root = ReactDom.createRoot(document.getElementById('app'))
