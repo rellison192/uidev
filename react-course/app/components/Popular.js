f0bc0c38a5ba21aa81aa75c7647dd42e080c2598
Ryan Ellison <rellison192@g.rwu.edu>
2022-09-30 21:29:23 -0400

    react course - 3 navbar state

--- react-course/app/components/Popular.js
+++ react-course/app/components/Popular.js
@@ -1,17 +1,33 @@
 import React from 'react'
 
 export default class Popular extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      selectedLanguage: 'All',
+    }
+    this.updateLanguage = this.updateLanguage.bind(this)
+  }
+  updateLanguage(selectedLanguage) {
+    this.setState({ selectedLanguage })
+  }
   render() {
     const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
-
     return (
-      <select>
-        {languages.map((language) => (
-          <option key={language} value={language}>
-            {language}
-          </option>
-        ))}
-      </select>
+      <main>
+        <select
+          value={this.state.selectedLanguage}
+          onChange={(event) => this.updateLanguage(event.target.value)}
+        >
+          {languages.map((language) => (
+            <option key={language} value={language}>
+              {language}
+            </option>
+          ))}
+        </select>
+        {JSON.stringify(this.state, null, 2)}
+      </main>
     )
   }
 }
