0b2d12cda26ea6b9ff23503d42b1249481a82eaa
Ryan Ellison <rellison192@g.rwu.edu>
2022-10-01 13:03:41 -0400

    react course - 5 languages nav proptypes

--- react-course/app/components/Nav.js
+++ react-course/app/components/Nav.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from "prop-types"
 
 export default function Nav({ selected, onUpdateLanguage }) {
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
@@ -15,4 +16,9 @@ export default function Nav({ selected, onUpdateLanguage }) {
       ))}
     </select>
   )
+}
+
+Nav.propTypes = {
+  selected: PropTypes.string.isRequired,
+  onUpdateLanguage: PropTypes.func.isRequired
 }
