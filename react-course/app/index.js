0cd530a69fbc2a35e7eb77f60b11605351692f18
Ryan Ellison <rellison192@g.rwu.edu>
2022-10-06 09:55:57 -0400

    react course - 17 routes

--- react-course/app/index.js
+++ react-course/app/index.js
@@ -2,16 +2,22 @@ import React from 'react'
 import ReactDom from 'react-dom/client'
 import Popular from './components/Popular'
 import Battle from './components/battle/Battle'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 
 class App extends React.Component {
   render() {
     return (
-      <div className="light">
-        <div className="container">
-          <Popular />
+      <BrowserRouter>
+        <div className="light">
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Popular />} />
+              <Route path="/battle" element={<Battle />} />
+            </Routes>
+          </div>
         </div>
-      </div>
+      </BrowserRouter>
     )
   }
 }
--- react-course/webpack.config.js
+++ react-course/webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index_bundle.js',
+    publicPath: "/",
   },
   module: {
     rules: [
@@ -21,4 +22,7 @@ module.exports = {
     }),
     new Dotenv(),
   ],
+  devServer: {
+    historyApiFallback: true,
+  },
 };
