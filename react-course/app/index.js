e62e6781891eda6a46a2ad6a7c63af62815f8df5
Ryan Ellison <rellison192@g.rwu.edu>
2022-10-07 12:59:46 -0400

    react course - 21 code split

--- react-course/app/index.js
+++ react-course/app/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDom from 'react-dom/client'
-import Popular from './components/popular/Popular'
-import Battle from './components/battle/Battle'
 import Nav from './components/Nav'
-import Results from './components/battle/Results'
+import Loading from './components/loading/Loading'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
+
+const Popular = React.lazy(() => import('./components/popular/Popular'))
+const Battle = React.lazy(() => import('./components/battle/Battle'))
+const Results = React.lazy(() => import('./components/battle/Results'))
 
 class App extends React.Component {
   state = {
@@ -22,11 +24,13 @@ class App extends React.Component {
         <div className={this.state.theme}>
           <div className="container">
             <Nav theme={this.state.theme} toggleTheme={this.toggleTheme} />
-            <Routes>
-              <Route path="/" element={<Popular />} />
-              <Route path="/battle" element={<Battle />} />
-              <Route path="/results" element={<Results />} />
-            </Routes>
+            <React.Suspense fallback={<Loading />}>
+              <Routes>
+                <Route path="/" element={<Popular />} />
+                <Route path="/battle" element={<Battle />} />
+                <Route path="/results" element={<Results />} />
+              </Routes>
+            </React.Suspense>
           </div>
         </div>
       </BrowserRouter>
